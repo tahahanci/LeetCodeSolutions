dead92bed63f1064b0f52c145662e93f51558ef6
Taha Hancı <taha.hanci@boun.edu.tr>
2024-03-08 00:04:00 +0300

    temp converted

---
+++ src/solutions/ConvertTemp.java
@@ -0,0 +1,11 @@
+package solutions;
+
+public class ConvertTemp {
+
+    public double[] convertTemperature(double celsius) {
+        double[] converted = new double[2];
+        converted[0] = celsius + 273.15;
+        converted[1] = celsius * 1.80 + 32.00;
+        return converted;
+    }
+}
